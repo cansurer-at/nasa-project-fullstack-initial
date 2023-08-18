bfea5925f7275e5402946825562ec145872f9104
Can Surer <cansurer@outlook.com>
2023-08-18 17:37:33 +0200

    refactor

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,34 +1,18 @@
 const API_URL = "http://localhost:8000";
 
 async function httpGetPlanets() {
-  try {
-    const response = await fetch(`${API_URL}/planets`);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return await response.json();
-  } catch (error) {
-    console.error('Error fetching planets:', error);
-    throw error;
-  }
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
 }
 
-httpGetPlanets()
-  .then(planets => {
-    // Process the planets data
-    console.log(planets);
-  })
-  .catch(error => {
-    // Handle errors
-    console.error(error);
-  });
-
-  
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
 }
-
 async function httpSubmitLaunch(launch) {
   // TODO: Once API is ready.
   // Submit given launch data to launch system.
--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const path = require("path");
-const morgan = require('morgan')
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router.js");
 
 app.use(express.json());
 
@@ -14,13 +15,13 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(planetsRouter);
-
-app.get("/", (req, res) => {
+app.use(launchesRouter);
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
