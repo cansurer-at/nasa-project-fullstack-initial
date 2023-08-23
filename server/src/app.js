d07fca712a9b4a2f3d9d1f5e17c12ad784a12d9d
Can Surer <cansurer@outlook.com>
2023-08-23 22:53:40 +0200

    change roots

--- server/src/app.js
+++ server/src/app.js
@@ -19,9 +19,9 @@ app.use(morgan("combined"));
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use(planetsRouter);
-app.use("/launches", launchesRouter);
-app.get("/*", (req, res) => {
+app.use('/planets', planetsRouter);
+app.use('/launches', launchesRouter);
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
