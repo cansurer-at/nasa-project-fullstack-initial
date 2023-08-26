9040402b68e1757d26e4f40291d5d22125bb3ab6
Can Surer <cansurer@outlook.com>
2023-08-26 20:20:36 +0200

    delete function

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -4,16 +4,27 @@ let latestFlightNumber = 100;
 
 const launch = {
   flightNumber: 100,
-  mission: 'Kepler Exploration X',
-  rocket: 'Explorer IS1',
-  launchDate: new Date('December 27, 2030'),
-  target: 'Kepler-442 b',
-  customer: ['ZTM', 'NASA'],
+  mission: "Kepler Exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 2030"),
+  target: "Kepler-442 b",
+  customer: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
 };
 
 launches.set(launch.flightNumber, launch);
+
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
+
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
+}
 
 function getAllLaunches() {
   return Array.from(launches.values());
@@ -33,6 +44,8 @@ function addNewLunch(launch) {
 }
 
 module.exports = {
+  existsLaunchWithId,
   getAllLaunches,
   addNewLunch,
+  abortLaunchById,
 };
