39d8d0a18e037ae967a4ae13e944c09d5856d935
Can Surer <cansurer@outlook.com>
2023-09-07 00:19:41 +0200

    connecting to the mongo-db and  hiding key

--- server/src/server.js
+++ server/src/server.js
@@ -2,13 +2,25 @@ const http = require("http");
 
 const app = require("./app");
 
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL = process.env.MONGO_URL || "default-mongo-url-here";
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("Connected to mongodb");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 }
 
